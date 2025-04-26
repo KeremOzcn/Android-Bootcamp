cfe8286f336756ef256e82a1242cefa4e6442e77
Kerem Özcan <139013257+KeremOzcn@users.noreply.github.com>
2025-04-26 17:02:31 +0300

    Updating files
    Ödev gönderimi

---
+++ HW1/Odev1.kt
@@ -0,0 +1,53 @@
+package com.example.firstproject.degiskenler
+
+// Ödev 1: Değişkenler ve değer atamaları
+
+val sehir = "Samsun"
+val ulke = "Türkiye"
+val telefon = "+90 545 123 45 67"
+val postaKodu = "52000"
+val email = "edanuraslanbas7@gmail.com"
+val meslek = "Backend Developer"
+val stokMiktari = 150
+val musteriAdi = "Edanur Aslanbaş"
+val bakiye = 1250.75
+val dogumGunu = "2006-10-23"
+val maas = 18000
+val medeniDurum = "Evli"
+val urunYorum = "Ürün beklediğimden iyi çıktı."
+
+val odemeTarihi = "2025-05-01"
+val odeme = 450.0
+val siparisAdeti = 3
+val arabaModeli = "Ferrari SF-24"
+val kitapAdi = "Süt ve Bal"
+val yayinlamaTarihi = "2023-11-20"
+val indirimMiktari = 15.5
+val odaSayisi = 4
+val enlem = 41.0082
+val boylam = 28.9784
+val urunAdi = "Eşek peluş oyuncak"
+val yemekFiyati = 95.0
+
+val marka = "Oppo"
+val muzikAdi = "Dünyanın en güzel kızı"
+val videoSuresi = 215
+val urunPuani = 4.7
+val resimAdi = "tatil_fotografi.jpg"
+val dosyaFormati = "JPEG"
+val renk = "Gri"
+val renkKodu = "#0000FF"
+val telefonModeli = "Oppo F3"
+val ekranBoyutu = 6.7
+val agirlik = 180
+
+val ulusalGun = "29 Ekim Cumhuriyet Bayramı"
+val tatilGunu = "Ramazan Bayramı"
+val rezervasyonTarihi = "2025-08-15"
+val sokakAdi = "Edanur Caddesi"
+val otobusHatti = "55T"
+val kalanDakika = 5
+val takipKodu = "TR1234567890"
+val kuponSuresi = 30
+val kuponKodu = "INDIRIM30"
+val faturaAdresi = "İstanbul, Fatih, 34710"
